75c530816ce93d77eebad3b3789212e4007d70e5
UCSCMattP <meprager+github@ucsc.edu>
2025-09-30 12:46:00 -0700

    Add files via upload

---
+++ #session 1 prompt.py
@@ -0,0 +1,16 @@
+#session 1 prompt 
+#program to print out your full name and 
+#2 sentences about fav movie and food 
+
+#this function tells the computer to print
+#the 
+def myname():
+	print("howdy my name is Matt Prager\nI like the movie Airplane because I think its the best PG movie of all time\nI love Chocolate chip cookies and have spent over 100 hours doing RnD on my recipie")
+
+#this defines our main function for the program 	
+def main(): 
+myname()
+
+#when the program is run this executes first
+if __name__=="__main__":
+	main()
